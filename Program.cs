b537819d99dc454c582e1586bb90988b9bcb6119
menly-ctrl <g.ravkoov@mail.ru>
2024-07-16 12:55:02 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System;
+
+
+namespace MyFirstProgram
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            PrintInfo();
+        }
+
+        static void PrintInfo()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
